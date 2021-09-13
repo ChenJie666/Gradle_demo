52fe397ab730bdace46b8f32a45585657a06cd61
CJ <792965772@qq.com>
2021-09-14 01:35:16 +0800

    Finish

---
+++ Mode/src/main/java/com/imooc/gradle/Mode.java
@@ -0,0 +1,9 @@
+package com.imooc.gradle;
+
+/**
+ * @author CJ
+ * @date 2021/9/13 22:12
+ */
+public class Mode {
+    private String name;
+}
---
+++ Repository/src/main/java/com/imooc/gradle/Repository.java
@@ -0,0 +1,13 @@
+package com.imooc.gradle;
+
+import com.imooc.gradle.Mode;
+
+/**
+ * @author CJ
+ * @date 2021/9/13 22:12
+ */
+public class Repository {
+
+    private Mode mode = new Mode();
+
+}
